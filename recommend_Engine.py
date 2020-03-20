e27326d6849f6453ef15748799a04557be2e589b
ruben <rubenski0345@gmail.com>
2020-03-20 20:32:12 +0100

    Originele file heeft wat errors gefixt gekregen

--- recommend_Engine.py
+++ recommend_Engine.py
@@ -9,48 +9,67 @@ namecursor = c.cursor() #de naamzoeker
 namelist = []
 relatiedictionary = {} #de dictionary waar alle relaties staan
 
+
 def nametolist():
     for name in cursor.fetchall() :
         name = name[-1]
-        if name != None:
+        if name != None and not "'" in name:
             namelist.append(name)
     return namelist
 
 def relatiedicmaker (namelist, relatiedictionary):
-    for name in namelist:
+    for name in namelist [:100]:
+        nummer = 0
         relatielist = namelist #om ervoor te zorgen dat dezelfde product geen relatie wordt
         relatielist.remove(name)
+            # brandchecker
         for relatie in relatielist:
-            if len(relatiedictionary) < 5: #het product krijgt maximaal 4 relaties
-                laadpunt = "category" #defineert wat de lader laad
+            if nummer < 5: #het product krijgt maximaal 4 relaties
+                laadpunt = "brand" #defineert wat de lader laad
                 sys.stdout.write('\r' + laadpunt + ' Relatie nummer ' + str(relatielist.index(relatie) + 1)+ "/"+ str(len(relatielist)) + " laden \n")
+                relatiecursor.execute("SELECT brand FROM products WHERE name ='" + str(relatie) + "';")
+                namecursor.execute("SELECT brand FROM products WHERE name='" + str(name) + "';")
+                namecategory = namecursor.fetchall()
+                namecategory = namecategory[-1]
+                relatiecategory = relatiecursor.fetchall()
+                if relatiecategory == namecategory and nummer < 5:
+                    nummer += 1
+                    relatiedictionary[str(nummer) + ". " + name] = relatie
+                # subcategorychecker
+        for relatie in relatielist[:100]:
+            if nummer < 5:  # het product krijgt maximaal 4 relaties
+                laadpunt = "subcategory"  # defineert wat de lader laad
+                sys.stdout.write('\r' + laadpunt + ' Relatie nummer ' + str(relatielist.index(relatie) + 1) + "/" + str(
+                    len(relatielist)) + " laden \n")
+                relatiecursor.execute("SELECT subcategory FROM products WHERE name ='" + str(relatie) + "';")
+                namecursor.execute("SELECT subcategory FROM products WHERE name='" + str(name) + "';")
+                namecategory = namecursor.fetchall()
+                namecategory = namecategory[-1]
+                for relatiecategory in relatiecursor.fetchall():
+                    if relatiecategory == namecategory and nummer < 5:
+                        nummer += 1
+                        relatiedictionary[str(nummer) + ". " + name] = relatie
+                        break
                 #categorychecker
-                relatiecursor.execute("SELECT category FROM products;")
+        for relatie in relatielist[:100]:
+            if nummer < 5:  # het product krijgt maximaal 4 relaties
+                laadpunt = "category"  # defineert wat de lader laad
+                sys.stdout.write('\r' + laadpunt + ' Relatie nummer ' + str(relatielist.index(relatie) + 1) + "/" + str(
+                    len(relatielist)) + " laden \n")
+                relatiecursor.execute("SELECT category FROM products WHERE name ='" + str(relatie) + "';")
                 namecursor.execute("SELECT category FROM products WHERE name='" + str(name) + "';")
                 namecategory = namecursor.fetchall()
                 namecategory = namecategory[-1]
                 for relatiecategory in relatiecursor.fetchall():
-                    if relatiecategory ==  namecategory:
-                        for nummer in range (1,4):
-                            relatiedictionary[str(nummer) + ". " + name] = relatie
+                    if relatiecategory ==  namecategory and nummer < 5:
+                        nummer += 1
+                        relatiedictionary[str(nummer) + ". " + name] = relatie
+                        break
 
 
 
-def progressbar(it, prefix="", size=60, file=sys.stdout):
-    count = len(it)
-    def show(j):
-        x = int(size*j/count)
-        file.write("%s[%s%s] %i/%i\r" % (prefix, "#"*x, "."*(size-x), j, count))
-        file.flush()
-    show(0)
-    for i, item in enumerate(it):
-        yield item
-        show(i+1)
-    file.write("\n")
-    file.flush()
 
 nametolist()
 relatiedicmaker (namelist, relatiedictionary)
 with open ("Datarelatiedict.json", "w") as outfile:
     json.dump(relatiedictionary, outfile)
-print(relatiedictionary)
